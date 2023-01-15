e60a7acb91f6e2947964d56471468ec19e664823
wanchaichab <wanchaichab@gmail.com>
2023-01-15 01:17:19 -0700

    First commit

---
+++ DiceFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Mission_2
+{
+	public class DiceFunctions
+	{
+		public void RollDice(int maxRolls)
+		{
+			Random r = new Random();
+			int[] totals = new int[11];
+
+			// Loop to roll 2 dice
+			for (int i = 0; i < maxRolls; i++)
+			{
+				int currentRoll = r.Next(1, 7);
+				currentRoll += r.Next(1, 7);
+				// Store current roll in array
+				totals[currentRoll - 2] += 1;
+				currentRoll = 0;
+			}
+
+			// Calculate percentage
+			for (int i = 0; i < totals.Length; i++)
+			{
+				float numStars = (totals[i] / (float)maxRolls) * 100;
+				string stars = "";
+
+				// Create stars
+				for (int j = 0; j < numStars; j++)
+				{
+					stars += "*";
+				}
+
+				// Print histogram
+				Console.WriteLine((i + 2) + ": " + stars);
+			}
+        }
+	}
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Mission_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "";
+            int rolls = 0;
+            DiceFunctions df;
+
+            // Get throw number
+            Console.WriteLine("Welcome to the dice throwing simulator!\n");
+            Console.Write("How many dice rolls would you like to simulate? ");
+            input = Console.ReadLine();
+            rolls = Convert.ToInt32(input);
+
+            Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+            Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+            Console.WriteLine("Total number of rolls = " + input + ".\n");
+
+            df = new DiceFunctions();
+            df.RollDice(rolls);
+
+            Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!\n");
+
+            
+        }
+    }
+}
+
